11a21b758a6577be3837f93403291600b56b9b50
Alan Fidelino <afidedev26@gmail.com>
2019-03-30 22:14:51 +1100

    Add form to CoursesPage.js

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,7 +1,43 @@
 import React, { Component } from "react";
 
-const CoursesPage = () => {
-  return <h2>Courses</h2>;
-};
+class CoursesPage extends Component {
+  state = {
+    course: { title: "" }
+  };
+
+  handleChange = event => {
+    const course = { ...this.state.course, title: event.target.value };
+
+    this.setState({ course });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    alert(this.state.course.title);
+  };
+  render() {
+    const margin = { marginBottom: "10px" };
+    return (
+      <>
+        <h2>Courses</h2>
+        <h3>Add a new course:</h3>
+        <div className="form-group">
+          <form onSubmit={this.handleSubmit} className="form">
+            <input
+              style={margin}
+              type="text"
+              value={this.state.course.title}
+              onChange={this.handleChange}
+              className="form-control"
+            />
+            <button style={margin} className="btn btn-success">
+              Save
+            </button>
+          </form>
+        </div>
+      </>
+    );
+  }
+}
 
 export default CoursesPage;
