c063d5cf4861a3ca3d74e863c1c259627579afb8
Alan Fidelino <afidedev26@gmail.com>
2019-03-31 13:55:04 +1100

    Wiring store to containers

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,4 +1,8 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import * as courseActions from "../../redux/actions/courseActions";
+import PropTypes from "prop-types";
+import { bindActionCreators } from "redux";
 
 class CoursesPage extends Component {
   state = {
@@ -7,32 +11,29 @@ class CoursesPage extends Component {
 
   handleChange = event => {
     const course = { ...this.state.course, title: event.target.value };
-
     this.setState({ course });
   };
 
   handleSubmit = event => {
     event.preventDefault();
-    alert(this.state.course.title);
+    this.props.actions.createCourse(this.state.course);
   };
   render() {
-    const margin = { marginBottom: "10px" };
     return (
       <>
         <h2>Courses</h2>
         <h3>Add a new course:</h3>
         <div className="form-group">
-          <form onSubmit={this.handleSubmit} className="form">
+          <form onSubmit={this.handleSubmit}>
             <input
-              style={margin}
               type="text"
               value={this.state.course.title}
               onChange={this.handleChange}
-              className="form-control"
             />
-            <button style={margin} className="btn btn-success">
-              Save
-            </button>
+            <input type="submit" value="Save" />
+            {this.props.courses.map(course => (
+              <div key={course.title}>{course.title}</div>
+            ))}
           </form>
         </div>
       </>
@@ -40,4 +41,23 @@ class CoursesPage extends Component {
   }
 }
 
-export default CoursesPage;
+function mapStateToProps(state) {
+  return {
+    courses: state.courses
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators(courseActions, dispatch)
+  };
+}
+
+CoursesPage.propTypes = {
+  actions: PropTypes.object.isRequired,
+  courses: PropTypes.array.isRequired
+};
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(CoursesPage);
